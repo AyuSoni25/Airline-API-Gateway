998817028449d45e3588178ddbe3bc5c91c367e4
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-04 19:12:15 +0530

    Feat: Added middlewares for auth

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const { UserController } = require('../../controllers');
-const { AirplaneMiddleware } = require('../../middlewares');
+const { AirplaneMiddleware, AuthRequestMiddlewares } = require('../../middlewares');
 
 const userRouter = express.Router();
 
 // /api/v1/user/signup POST
-userRouter.post('/signup', UserController.signup);
+userRouter.post('/signup', AuthRequestMiddlewares.validateAuthRequest, UserController.signup);
 
 // /api/v1/user/signin POST
-userRouter.post('/signin', UserController.signin);
+userRouter.post('/signin', AuthRequestMiddlewares.validateAuthRequest, UserController.signin);
 
 module.exports = userRouter;
