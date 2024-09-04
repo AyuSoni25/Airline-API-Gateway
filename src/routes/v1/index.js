b3eefbc7c9937fa3b6af56749a91df55748f88a7
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-04 19:29:01 +0530

    Feat: Added isAuthenticated middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { InfoController } = require('../../controllers');
 const userRouter = require('./user.routes');
+const { AuthRequestMiddlewares } = require('../../middlewares');
 
 const v1Router = express.Router();
 
-v1Router.get('/info', InfoController.info);
+v1Router.get('/info', AuthRequestMiddlewares.checkAuth, InfoController.info);
 v1Router.use('/user', userRouter);
 
 module.exports = v1Router;
