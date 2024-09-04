43a9bc723df59c4ddf229f3ced6c2147ffe946f3
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-04 23:20:20 +0530

    Feat:Added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
 const { ServerConfig } = require('./config');
+const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const apiRoutes = require('./routes');
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 3, // Limit each IP to 2 requests per `window` (here, per 15 minutes)
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
+
+app.use('/flightsService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, pathRewrite: {'^/flightsService' : '/'} }));
+app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true }));
 
 app.use('/api', apiRoutes);
 
